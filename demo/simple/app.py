886a542c56f215ef16da2fa92cf9b3ed2564c740
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2014-12-14 22:51:11 -0200

    Typo ...

--- demo/simple/app.py
+++ demo/simple/app.py
@@ -28,10 +28,11 @@ class Simple(object):
     
 
 if __name__ == '__main__':
-    app     = RapidServ('0.0.0.0', 5000, 60)
+    app = RapidServ('0.0.0.0', 5000, 60)
 
     app.add_handle(Simple)
     core.gear.mainloop()
     
+
 
 
